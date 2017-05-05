f7f6973c58a9229ba74d82070212b7e37550f211
CyrilHskt <c.hasquette@gmail.com>
2017-05-05 11:55:30 +0200

    Adding random citation function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 // Code React
 import React from 'react';
 import { render } from 'react-dom';
+import citations from './citations';
 import './index.css';
 
 class App extends React.Component {
-    render() {
+    
+    state = {};
+
+    genererCitation = event => {
+        const keyArray = Object.keys(citations);
+        const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)];
+        this.setState(citations[randomKey]);
+    };
+
+    render(){
         return (
             <div>
-                <p>Ma Citation
-                <span>- Auteur</span>
+                <p>{this.state.citation}
+                <span>- {this.state.auteur}</span>
                 </p>
-                <button>Une autre citation</button>
+                <button onClick={e => this.genererCitation(e)}>Une autre citation</button>
             </div>
         )
-    }
+    };
 }
 
 render(
