88090c56c81ffc8fe75f28e94edcd97708293eaf
CyrilHskt <c.hasquette@gmail.com>
2017-05-05 11:17:36 +0200

    First component and CSS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 // Code React
 import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Ma Citation
+                <span>- Auteur</span>
+                </p>
+                <button>Une autre citation</button>
+            </div>
+        )
+    }
+}
+
+render(
+    <App />,
+    document.getElementById('root')
+)
